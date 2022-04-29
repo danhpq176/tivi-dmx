64b58054d2971a441caf0fc193a3a0257b50e7e4
danhpq176 <danhpq176@github.com>
2022-04-29 18:43:31 +0700

    upcode

--- src/components/IncomingCall/index.js
+++ src/components/IncomingCall/index.js
@@ -8,7 +8,7 @@ export const IncomingCall = () => {
 			<div onClick={() => window.open('tel:02822432939', '_self')} className=' call-animation'>
 				<PhoneInTalkIcon />
 			</div>
-			<span className='phone'>02822432939</span>
+			<span className='phone'>02822432939 </span>
 		</div>
 	)
 }
